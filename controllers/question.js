13907008950d22ece3e158a4f235e842813bb67a
Varick Lim <valishru@hotmail.com>
2018-11-11 15:20:39 +0800

    Answers in getQuestionById include vote count

--- controllers/question.js
+++ controllers/question.js
@@ -1,5 +1,8 @@
 const Question = require('../models/Question');
 const Answer = require('../models/Answer');
+const AnswerVote = require('../models/AnswerVote');
+
+const moment = require('moment');
 
 /**
  * GET /questions/:questionId
@@ -8,9 +11,31 @@ const Answer = require('../models/Answer');
 exports.getQuestionById = async (req, res) => {
   const question_id = req.params.questionId;
   const question = await Question.findById(question_id);
-  const answers = await Answer.find({ question_id });
+  let answers = await Answer
+    .find({ question_id })
+    .populate('user_id', '_id email')
+    .exec();
+
+  const voteCounts = await AnswerVote.aggregate([
+    { $group: {
+      _id: '$answer_id',
+      value: { $sum: 1 },
+    }}
+  ]);
+  answers = answers.map((answer) => {
+    // Convert MongoDB document to JSON object
+    answerObj = answer.toObject();
+    // Add vote count
+    const voteCount = voteCounts.find(count => 
+      (count._id.toString() == answerObj._id.toString())
+    );
+    answerObj.votes = voteCount ? voteCount.value : 0;
+    // Parse date
+    answerObj.createdAt = moment(answerObj.createdAt).format("D MMMM YYYY");
+    return answerObj;
+  });
   res.render('question/question', {
     question: question,
-    answers: answers
+    answers: answers.sort((a,b) => b.votes - a.votes)
   });
 };
