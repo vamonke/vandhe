714410d764e60e945cc6adc05da518813659d7ad
Unknown <dhea23mar@gmail.com>
2018-11-13 16:58:19 +0800

    update

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ const expressValidator = require('express-validator');
 const expressStatusMonitor = require('express-status-monitor');
 const sass = require('node-sass-middleware');
 const multer = require('multer');
-
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
 /**
