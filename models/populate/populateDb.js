5c2f61fc800e739b911256a2e5ceba1f9879ba62
Unknown <dhea23mar@gmail.com>
2018-11-14 16:51:50 +0800

    change db at populatedb too

--- models/populate/populateDb.js
+++ models/populate/populateDb.js
@@ -11,7 +11,7 @@ const Reply = require('../Reply');
 const mongoose = require('mongoose');
 
 // console.log('Connecting to MongoDB..');
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+mongoose.connect('mongodb://vandhe:vandhe123@ds163013.mlab.com:63013/heroku_dwnhstmt', { useNewUrlParser: true });
 
 const lorem_ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
